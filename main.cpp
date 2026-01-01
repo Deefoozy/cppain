66e2dd30932a18048611676ef7a428c97c69b8bd
Deefoozy <tinotrok@outlook.com>
2026-01-01 14:04:17 +0100

    Added some base code to build on top of

--- main.cpp
+++ main.cpp
@@ -1,6 +1,69 @@
+#include <vulkan/vulkan_core.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+
 #include <iostream>
 
-int main() {
+class VisApp {
+public:
+  void run() {
+    // TODO: make myself very happy by also doing this shit multi threaded
+    createWindow();
+
+    mainLoop();
+
+    cleanup();
+  }
+
+private:
+  bool hasWindow_val = false;
+  bool *hasWindow = &hasWindow_val;
+
+  bool glfwIsReady_val = false;
+  bool *glfwIsReady = &glfwIsReady_val;
+
+  // hehe
+  GLFWwindow *windows;
+
+  void createWindow() {
+    if (*this->hasWindow) {
+      return;
+    }
+
+    if (!*this->glfwIsReady) {
+      glfwInit();
+      *this->glfwIsReady = true;
+    }
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    this->windows =
+        glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    *this->hasWindow = true;
+  }
+
+  void mainLoop() {
     std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    while (!glfwWindowShouldClose(this->windows)) {
+      glfwPollEvents();
+    }
+  }
+
+  void cleanup() {
+    glfwDestroyWindow(this->windows);
+
+    glfwTerminate();
+  }
+};
+
+int main() {
+  VisApp mainApplication;
+
+  mainApplication.run();
+
+  return 0;
 }
