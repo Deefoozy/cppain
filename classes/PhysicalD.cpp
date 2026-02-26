c0f260384c20c281ca65f77d4b6608492d4345b5
Deefoozy <tinotrok@outlook.com>
2026-02-26 17:48:39 +0100

    Updated PhysicalD to contain extension checks

--- classes/PhysicalD.cpp
+++ classes/PhysicalD.cpp
@@ -1,9 +1,14 @@
 #include <vulkan/vulkan_core.h>
 
+#include <set>
 #include <vector>
 #include <iostream>
 
 #include "PhysicalD.h"
+
+const std::vector<const char*> PhysicalD::requiredDeviceExtensions = {
+  VK_KHR_SWAPCHAIN_EXTENSION_NAME
+};
 
 uint32_t PhysicalD::getPhysicalDCount(const VkInstance& instance) {
   uint32_t deviceCount = 0;
@@ -23,7 +28,7 @@ uint32_t PhysicalD::getPhysicalDCount(const VkInstance& instance) {
   return deviceCount;
 }
 
-uint32_t PhysicalD::getPhysicalDs(const VkInstance& instance, uint32_t deviceCount, std::vector<VkPhysicalDevice>& physicalDs) {
+void PhysicalD::getPhysicalDs(const VkInstance& instance, uint32_t deviceCount, std::vector<VkPhysicalDevice>& physicalDs) {
   std::cout << "Physical Vulkan device count: " << deviceCount << std::endl;
 
   const VkResult status = vkEnumeratePhysicalDevices(
@@ -37,8 +42,6 @@ uint32_t PhysicalD::getPhysicalDs(const VkInstance& instance, uint32_t deviceCou
 
     throw std::runtime_error("Encountered issue while getting device_count.");
   }
-
-  return deviceCount;
 }
 
 bool PhysicalD::deviceIsViable(const VkPhysicalDevice& physicalD) {
@@ -55,22 +58,64 @@ bool PhysicalD::deviceIsViable(const VkPhysicalDevice& physicalD) {
     &features
   );
 
-  std::cout << "Device name: " << properties.deviceName << std::endl;
+  std::cout << "Device name: " << properties.deviceName << " || ";
 
   if (
-    properties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU ||
-    properties.deviceType == VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU
+    properties.deviceType != VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU &&
+    properties.deviceType != VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU
   ) {
-    std::cout << "Device is the right type (either a physical or integrated GPU)" << std::endl;
+    std::cout << "Device is not of the right type. (must be either a physical or integrated GPU)" << std::endl;
 
-    if (features.geometryShader) {
-      std::cout << "Device has a geometry shader" << std::endl;
-
-      return true;
-    }
+    return false;
   }
 
-  return false;
+  if (!features.geometryShader) {
+    std::cout << "Device does not have a geometry shader" << std::endl;
+
+    return false;
+  }
+
+  if (!checkExtensionSupport(physicalD)) {
+    std::cout << "Device does not suppoort all required extensions" << std::endl;
+
+    return false;
+  }
+
+  std::cout << "Device viable." << std::endl;
+  return true;
+}
+
+bool PhysicalD::checkExtensionSupport(const VkPhysicalDevice& physicalD) {
+  const uint32_t extensionCount = PhysicalD::getExtensionCount(physicalD);
+
+  std::vector<VkExtensionProperties> availableExtensions(extensionCount);
+
+  getExtensions(physicalD, extensionCount, availableExtensions);
+
+  std::set<std::string> tempRequiredExtensions(requiredDeviceExtensions.begin(), requiredDeviceExtensions.end());
+
+  for (const VkExtensionProperties& extension : availableExtensions) {
+    tempRequiredExtensions.erase(extension.extensionName);
+  }
+
+  return tempRequiredExtensions.empty();
+}
+
+uint32_t PhysicalD::getExtensionCount(const VkPhysicalDevice& physicalD) {
+  uint32_t extensionCount = 0;
+
+  vkEnumerateDeviceExtensionProperties(physicalD, nullptr, &extensionCount, nullptr);
+
+  return extensionCount;
+}
+
+void PhysicalD::getExtensions(const VkPhysicalDevice& physicalD, uint32_t extensionCount, std::vector<VkExtensionProperties>& availableExtensions) {
+  vkEnumerateDeviceExtensionProperties(
+    physicalD,
+    nullptr,
+    &extensionCount,
+    availableExtensions.data()
+  );
 }
 
 void PhysicalD::selectDevice(VkPhysicalDevice& physicalDevice, std::vector<VkPhysicalDevice>& physicalDs) {
--- classes/PhysicalD.h
+++ classes/PhysicalD.h
@@ -2,11 +2,16 @@
 #define CPPAIN_PHYSICALD_H
 
 class PhysicalD {
+private:
+  static const std::vector<const char*> requiredDeviceExtensions;
 public:
   static uint32_t getPhysicalDCount(const VkInstance& instance);
-  static uint32_t getPhysicalDs(const VkInstance& instance, uint32_t deviceCount, std::vector<VkPhysicalDevice>& physicalDs);
+  static void getPhysicalDs(const VkInstance& instance, uint32_t deviceCount, std::vector<VkPhysicalDevice>& physicalDs);
   static bool deviceIsViable(const VkPhysicalDevice& physicalD);
   static void selectDevice(VkPhysicalDevice& physicalDevice, std::vector<VkPhysicalDevice>& physicalDs);
+  static bool checkExtensionSupport(const VkPhysicalDevice& physicalD);
+  static uint32_t getExtensionCount(const VkPhysicalDevice& physicalD);
+  static void getExtensions(const VkPhysicalDevice& physicalD, uint32_t extensionCount, std::vector<VkExtensionProperties>& availableExtensions);
 };
 
 #endif // CPPAIN_PHYSICALD_H
